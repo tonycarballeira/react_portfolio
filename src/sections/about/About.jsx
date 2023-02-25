71e5fe97a7e5da7b56528f96b58b1f4c82451382
tonycarballeira <tonycarballeira@gmail.com>
2023-02-25 15:11:48 -0500

    modified styling for about me section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -8,19 +8,25 @@ const About = () => {
   return (
     <section id="about">
        <div className="container about__container">
-          <div className="about__left">
-            <div className="about__portrait">
-              <img src={Logo} alt="portrait" />
+        
+            <div className="header_div">
+              <h2 className='about_header'>About me</h2>
             </div>
-          </div>
-          <div className="about__right">
-            <h2>About me</h2>
-            <div className='about__cards'></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <a href={CV} download className='btn primary' target="blank" rel='noopener noreferrer'>Download CV<ImDownload/></a>
-          </div>
-       </div>
+            <div className="inner">
+              <div className="about__left">          
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                {/* <a href={CV} download className='btn primary' target="blank" rel='noopener noreferrer'>Download CV<ImDownload/></a> */}
+              </div>
+
+              <div className="about__right">
+                <div className="about__portrait">
+                  <img src={Logo} alt="portrait" />
+                </div>
+              </div>
+            </div>
+          </div> 
+        
     </section>
   )
 }
